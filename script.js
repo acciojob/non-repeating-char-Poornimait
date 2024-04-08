4dbf308fff553c42e952ea3e47fc22d07f568294
poornima reddy <54319456+Poornimait@users.noreply.github.com>
2024-04-08 09:54:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let count=0;
+	for(let i=0;i<str.length;i++){
+		if(str.charAt(i)===str.charAt(i+1)){
+			count++;
+			i++;
+		}else{
+			return str.charAt(i);
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
